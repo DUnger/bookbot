194ade4f8c5f3a245f432d64fe8c98739f272e1e
DUnger <daniel.unger90@web.de>
2025-04-18 16:32:44 +0200

    quick fix of the function counting the number of character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import get_num_words, count_num_chars_dict
 
-file_path = "books/frankenstein.txt"
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,6 +8,7 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
+    file_path = "books/frankenstein.txt"
     book_text = get_book_text(file_path)
     num_words = get_num_words(book_text)
     chars_dict = count_num_chars_dict(book_text)
--- stats.py
+++ stats.py
@@ -7,10 +7,11 @@ def count_num_chars_dict(book_text):
     chars_dict = {}
     for char_origin in book_text:
         char = char_origin.lower()
-        if char == " ":
-            continue
-        elif char in chars_dict:
+        if char in chars_dict:
             chars_dict[char] += 1
         else:
             chars_dict[char] = 1
     return chars_dict
+
+def sort_chars_dict(chars_dict):
+    return chars_dict.sort
