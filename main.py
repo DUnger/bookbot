575e95a1305825f2ebe8aac5f1ed614bc004ea78
DUnger <daniel.unger90@web.de>
2025-04-18 16:24:13 +0200

    statistics of BookBot done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_num_chars_dict
 
 file_path = "books/frankenstein.txt"
 
@@ -10,7 +10,8 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text(file_path)
     num_words = get_num_words(book_text)
+    chars_dict = count_num_chars_dict(book_text)
     print(f"{num_words} words found in the document")
-
+    print(chars_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_words(book_text):
     text_list = book_text.split()
     num_words = len(text_list)
     return num_words
+
+def count_num_chars_dict(book_text):
+    chars_dict = {}
+    for char_origin in book_text:
+        char = char_origin.lower()
+        if char == " ":
+            continue
+        elif char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
