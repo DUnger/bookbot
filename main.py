2746293b2a47badf2d0bc57c71189acd79a8641d
DUnger <daniel.unger90@web.de>
2025-11-12 18:07:27 +0100

    bookbot finished

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import get_num_words, get_num_chars, get_sorted_list_dicts
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     num_words_in_book = get_num_words(book_text)
     chars = get_num_chars(book_text)
     sorted_list = get_sorted_list_dicts(chars)
