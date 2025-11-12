bf92cb586fb2fea1eeb852d7b5dc653b463a1b0d
DUnger <daniel.unger90@web.de>
2025-11-12 18:01:59 +0100

    reporting finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, get_sorted_list_dicts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +8,17 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words_in_book = get_num_words(book_text)
     chars = get_num_chars(book_text)
+    sorted_list = get_sorted_list_dicts(chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words_in_book} total words")
-    print(chars)
+    print("--------- Character Count -------")
+
+    for dict in sorted_list:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def get_num_chars(book):
         else:
             chars[lower_char] = 1
     return chars 
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_list_dicts(chars_dict):
+    sorted_list = []
+    for char in chars_dict:
+        sorted_list.append({"char": char, "num": chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
