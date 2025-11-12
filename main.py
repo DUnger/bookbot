ac8ddebfba71687dcb2d8c848f646b21fd548cca
DUnger <daniel.unger90@web.de>
2025-11-12 17:19:30 +0100

    counts words in book

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
+def count_words(book):
+    words = book.split()
+    return len(words)   
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words_in_book = count_words(book_text)
+    print(f"Found {num_words_in_book} total words")
 
 main()
